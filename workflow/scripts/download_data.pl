e3220ad011615a2f6ef96303c98c5113d019a571
Brian O'Connor <briandoconnor@gmail.com>
2014-02-03 17:44:38 +0100

    Removed old symlinks, working on data download process

--- workflow/scripts/download_data.pl
+++ workflow/scripts/download_data.pl
@@ -4,24 +4,31 @@ use strict;
 
 my ($link_dir) = @ARGV;
 
-download("$link_dir/reference/bwa-0.6.2", "https://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz");
-download("$link_dir/reference/bwa-0.6.2", "https://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.fai");
-download("$link_dir/reference/bwa-0.6.2", "https://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.64.amb");
-download("$link_dir/reference/bwa-0.6.2", "https://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.64.ann");
-download("$link_dir/reference/bwa-0.6.2", "https://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.64.bwt");
-download("$link_dir/reference/bwa-0.6.2", "https://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.64.pac");
-download("$link_dir/reference/bwa-0.6.2", "https://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.64.sa");
-#download("$link_dir/reference/bwa-0.6.2", "https://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.64.sa");
+download("$link_dir/reference/bwa-0.6.2", "http://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz");
+download("$link_dir/reference/bwa-0.6.2", "http://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.fai");
+download("$link_dir/reference/bwa-0.6.2", "http://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.64.amb");
+download("$link_dir/reference/bwa-0.6.2", "http://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.64.ann");
+download("$link_dir/reference/bwa-0.6.2", "http://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.64.bwt");
+download("$link_dir/reference/bwa-0.6.2", "http://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.64.pac");
+download("$link_dir/reference/bwa-0.6.2", "http://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.64.sa");
+download("$link_dir/testData", "http://s3.amazonaws.com/oicr.bundle.data/pancancer_bwa_workflow/test.genome.read1.fastq.gz");
+download("$link_dir/testData", "http://s3.amazonaws.com/oicr.bundle.data/pancancer_bwa_workflow/test.genome.read2.fastq.gz");
 
 sub download {
   my ($dir, $url) = @_;
   system("mkdir -p $dir");
   $url =~ /\/([^\/]+)$/;
   my $file = $1;
-  if (!-e "$dir/$file" || -l "$dir/$file") {
+  if (!-e "$dir/$file" || -l "$dir/$file" || -s "$dir/$file" == 0) {
     my $cmd = "wget $url -O $dir/$file"; 
-    print "DOWNLOADING: $cmd FILE $file\n";
-    system($cmd);
+    print "\nDOWNLOADING: $cmd\nFILE: $file\n\n";
+    my $r = system($cmd);
+    if ($r != 0) {
+      print "+DOWNLOAD FAILED!\n";
+      $cmd = "lwp-download $url $dir/$file";
+      print "\nDOWNLOADING AGAIN: $cmd\nFILE: $file\n\n";
+      my $r = system($cmd);
+    }
   }
 }
 
