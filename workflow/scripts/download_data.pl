9fdf5f4f2d70151c64bbf5fcfe29350c0f3018f6
Brian O'Connor <briandoconnor@gmail.com>
2014-03-02 10:55:22 -0500

    Just working on the download script to include resume (if wget is present)

--- workflow/scripts/download_data.pl
+++ workflow/scripts/download_data.pl
@@ -4,6 +4,8 @@ use strict;
 
 my ($link_dir) = @ARGV;
 
+check_tools();
+
 download("$link_dir/reference/bwa-0.6.2", "http://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz");
 download("$link_dir/reference/bwa-0.6.2", "http://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.fai");
 download("$link_dir/reference/bwa-0.6.2", "http://s3.amazonaws.com/pan-cancer-data/pan-cancer-reference/genome.fa.gz.64.amb");
@@ -21,16 +23,33 @@ sub download {
   system("mkdir -p $dir");
   $url =~ /\/([^\/]+)$/;
   my $file = $1;
-  if (!-e "$dir/$file" || -l "$dir/$file" || -s "$dir/$file" == 0) {
-    my $cmd = "wget $url -O $dir/$file"; 
+
+  print "\nDOWNLOADING HEADER:\n\n";
+  my $r = `curl -I $url | grep Content-Length`;
+  $r =~ /Content-Length: (\d+)/;
+  my $size = $1;
+  print "\n+REMOTE FILE SIZE: $size\n";
+  my $fsize = -s "$dir/$file";
+  print "+LOCAL FILE SIZE: $size\n";
+
+  if (!-e "$dir/$file" || -l "$dir/$file" || -s "$dir/$file" == 0 || -s "$dir/$file" != $size) {
+    my $cmd = "wget -c -O $dir/$file $url"; 
     print "\nDOWNLOADING: $cmd\nFILE: $file\n\n";
     my $r = system($cmd);
-    if ($r != 0) {
+    if ($r) {
       print "+DOWNLOAD FAILED!\n";
       $cmd = "lwp-download $url $dir/$file";
       print "\nDOWNLOADING AGAIN: $cmd\nFILE: $file\n\n";
       my $r = system($cmd);
+      if ($r) {
+        die ("+SECOND DOWNLOAD FAILED! GIVING UP!\n");
+      }
     }
   }
 }
 
+sub check_tools {
+  if (system("which curl") || (system("which lwp-download") && system("which wget"))) {
+    die "+TOOLS NOT FOUND: Can't find curl and/or one of lwp-download and wget, please make sure these are installed and in your path!\n";
+  }
+}
