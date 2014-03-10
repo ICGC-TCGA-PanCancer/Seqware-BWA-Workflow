2a8ea56d896f17366ddecc2113d84fbfc37e57d6
Brian O'Connor <briandoconnor@gmail.com>
2014-03-10 17:04:50 -0400

    The decider is mostly working now, able to figure out what to run. Next step is to prepare the ini file and schedule the workflow.

--- workflow/scripts/workflow_decider_ebi.pl
+++ workflow/scripts/workflow_decider_ebi.pl
@@ -16,7 +16,7 @@ use XML::LibXML;
 # VARIABLES #
 #############
 
-my $down = 1;
+my $down = 0;
 my $gnos_url = "https://gtrepo-ebi.annailabs.com";
 my $cluster_json = "cluster.json";
 my $working_dir = "decider_tmp";
@@ -36,15 +36,80 @@ GetOptions("gnos-url=s" => \$gnos_url, "cluster-json=s" => \$cluster_json, "work
 
 # READ CLUSTER INFO AND RUNNING SAMPLES
 my ($cluster_info, $running_samples) = read_cluster_info($cluster_json);
-print Dumper($cluster_info);
-print Dumper($running_samples);
+#print Dumper($cluster_info);
+#print Dumper($running_samples);
 
 # READ INFO FROM GNOS
 my $sample_info = read_sample_info();
-print Dumper($sample_info);
+#print Dumper($sample_info);
 
 # FIND SAMPLES
 # now look at each sample, see if it's already schedule, launch if not and a cluster is available, and then exit
+print "SAMPLE SCHEDULING INFORMATION\n\n";
+foreach my $participant (keys %{$sample_info}) {
+  print "PARTICIPANT: $participant\n\n";
+  foreach my $sample (keys %{$sample_info->{$participant}}) {
+    # storing some info
+    my $d = {};
+    my $aligns = {};
+    print "\tSAMPLE OVERVIEW\n";
+    print "\tSAMPLE: $sample\n";
+    foreach my $alignment (keys %{$sample_info->{$participant}{$sample}}) {
+      print "\t\tALIGNMENT: $alignment\n";
+      $aligns->{$alignment} = 1;
+      foreach my $aliquot (keys %{$sample_info->{$participant}{$sample}{$alignment}}) {
+        print "\t\t\tALIQUOT: $aliquot\n";
+        foreach my $library (keys %{$sample_info->{$participant}{$sample}{$alignment}{$aliquot}}) {
+          print "\t\t\t\tLIBRARY: $library\n";
+          #print "$participant\t$sample\t$alignment\t$aliquot\t$library\n";
+          # read lane counts
+          my $total_lanes = 0;
+          foreach my $lane (keys %{$sample_info->{$participant}{$sample}{$alignment}{$aliquot}{$library}{total_lanes}}) {
+            if ($lane > $total_lanes) { $total_lanes = $lane; }
+          }
+          $d->{total_lanes_hash}{$total_lanes} = 1;
+          $d->{total_lanes} = $total_lanes;
+          foreach my $bam (keys %{$sample_info->{$participant}{$sample}{$alignment}{$aliquot}{$library}{files}}) {
+            $d->{bams}{$bam} = 1;
+            $d->{bams_count}++;
+          }
+          # analysis
+          foreach my $analysis (sort keys %{$sample_info->{$participant}{$sample}{$alignment}{$aliquot}{$library}{analysis_id}}) {
+            $d->{analysisURL}{"$gnos_url/cghub/metadata/analysisFull/$analysis"} = 1;
+          }
+          print "\t\t\t\t\tBAMS: ", join(",", (keys %{$sample_info->{$participant}{$sample}{$alignment}{$aliquot}{$library}{files}})), "\n\n";
+        }
+      }
+    }
+    print "\tSAMPLE WORKLFOW ACTION OVERVIEW\n";
+    print "\t\tLANES SPECIFIED FOR SAMPLE: $d->{total_lanes}\n";
+    #print Dumper($d->{total_lanes_hash});
+    print "\t\tBAMS FOUND: $d->{bams_count}\n";
+    #print Dumper($d->{bams});
+    my $veto = 0;
+    # so, do I run this?
+    if (scalar(keys %{$aligns}) == 1 && defined($aligns->{unaligned})) { print "\t\tONLY UNALIGNED!\n"; }
+    else { print "\t\tCONTAINS ALIGNMENT!\n"; $veto = 1; }
+    # now check if this is alreay scheduled
+    my $analysis_url_str = join(",", sort(keys(%{$d->{analysisURL}})));
+    #print "ANALYSISURL $analysis_url_str\n";
+    if (!defined($running_samples->{$analysis_url_str})) {
+      print "\t\tNOT PREVIOUSLY SCHEDULED!\n";
+    } else {
+      print "\t\tIS PREVIOUSLY SCHEDULED, RUNNING, OR FAILED!\n";
+      $veto = 1; 
+    }
+    # now check the number of bams == lane count (or this check is suppressed) 
+    if ($d->{total_lanes} == $d->{bams_count} || $ignore_lane_cnt) {
+      print "\t\tLANE COUNT MATCHES OR IGNORED: $ignore_lane_cnt $d->{total_lanes} $d->{bams_count}\n";
+    } else {
+      print "\t\tLANE COUNT MISMATCH!\n";
+      $veto=1;
+    }
+    if ($veto) { print "\t\tWILL NOT SCHEDULE THIS SAMPLE FOR ALIGNMENT!\n\n"; }
+    else { print "\t\tSCHEDULING WORKFLOW FOR THIS SAMPLE!\n\n"; }
+  }
+}
 
 
 ###############
@@ -53,6 +118,7 @@ print Dumper($sample_info);
 
 sub read_sample_info {
   
+  open OUT, ">xml_parse.log" or die;
   my $d = {};
 
   # PARSE XML
@@ -60,30 +126,33 @@ sub read_sample_info {
   #my $doc = $parser->parsefile ("https://cghub.ucsc.edu/cghub/metadata/analysisDetail?participant_id=3f70c3e3-0131-466f-92aa-0a63ab3d4258");
   #system("lwp-download 'https://cghub.ucsc.edu/cghub/metadata/analysisDetail?study=TCGA_MUT_BENCHMARK_4&state=live' data.xml");
   #my $doc = $parser->parsefile ('https://cghub.ucsc.edu/cghub/metadata/analysisDetail?study=TCGA_MUT_BENCHMARK_4&state=live');
-  if ($down) { my $cmd = "mkdir -p xml; cgquery -s $gnos_url -o xml/data.xml 'study=*&state=live'"; print "$cmd\n"; system($cmd); }
+  if ($down) { my $cmd = "mkdir -p xml; cgquery -s $gnos_url -o xml/data.xml 'study=*&state=live'"; print OUT "$cmd\n"; system($cmd); }
   my $doc = $parser->parsefile("xml/data.xml");
   
-  # print all HREF attributes of all CODEBASE elements
+  # print OUT all HREF attributes of all CODEBASE elements
   my $nodes = $doc->getElementsByTagName ("Result");
   my $n = $nodes->getLength;
+
+  # DEBUG
+  #$n = 30;
   
-  print "\n";
+  print OUT "\n";
   
   for (my $i = 0; $i < $n; $i++)
   {
       my $node = $nodes->item ($i);
       #$node->getElementsByTagName('analysis_full_uri')->item(0)->getAttributeNode('errors')->getFirstChild->getNodeValue;
-      #print $node->getElementsByTagName('analysis_full_uri')->item(0)->getFirstChild->getNodeValue;
+      #print OUT $node->getElementsByTagName('analysis_full_uri')->item(0)->getFirstChild->getNodeValue;
       my $aurl = getVal($node, "analysis_full_uri"); # ->getElementsByTagName('analysis_full_uri')->item(0)->getFirstChild->getNodeValue;
       # have to ensure the UUID is lower case, known GNOS issue
-      #print "Analysis Full URL: $aurl\n";
+      #print OUT "Analysis Full URL: $aurl\n";
       if($aurl =~ /^(.*)\/([^\/]+)$/) {
       $aurl = $1."/".lc($2);
       } else { 
-        print "SKIPPING!\n";
+        print OUT "SKIPPING!\n";
         next;
       }
-      print "ANALYSIS FULL URL: $aurl\n";
+      print OUT "ANALYSIS FULL URL: $aurl\n";
       #if ($down) { system("wget -q -O xml/data_$i.xml $aurl"); }
       if ($down) { download($aurl, "xml/data_$i.xml"); }
       my $adoc = $parser->parsefile ("xml/data_$i.xml");
@@ -91,45 +160,70 @@ sub read_sample_info {
       my $analysisId = getVal($adoc, 'analysis_id'); #->getElementsByTagName('analysis_id')->item(0)->getFirstChild->getNodeValue;
       my $analysisDataURI = getVal($adoc, 'analysis_data_uri'); #->getElementsByTagName('analysis_data_uri')->item(0)->getFirstChild->getNodeValue;
       my $submitterAliquotId = getCustomVal($adoc2, 'submitter_aliquot_id');
-      my $aliquotId = getVal($adoc, 'aliquot_id');
+      my $aliquotId = getCustomVal($adoc2, 'aliquot_id');
       my $submitterParticipantId = getCustomVal($adoc2, 'submitter_participant_id');
-      my $participantId = getVal($adoc, 'participant_id');
+      my $participantId = getCustomVal($adoc2, 'participant_id');
       my $submitterSampleId = getCustomVal($adoc2, 'submitter_sample_id');
-      my $sampleId = getVal($adoc, 'sample_id');
-      print "ANALYSIS:  $analysisDataURI \n";
-      print "ANALYSISID: $analysisId\n";
-      print "PARTICIPANT ID: $participantId\n";
-      print "SAMPLE ID: $sampleId\n";
-      print "ALIQUOTID: $aliquotId\n";
-      print "SUBMITTER PARTICIPANT ID: $submitterParticipantId\n";
-      print "SUBMITTER SAMPLE ID: $submitterSampleId\n";
-      print "SUBMITTER ALIQUOTID: $submitterAliquotId\n";
+      my $sampleId = getCustomVal($adoc2, 'sample_id');
+      my $use_control = getCustomVal($adoc2, "use_cntl");
+      my $alignment = getVal($adoc, "refassem_short_name");
+      my $total_lanes = getCustomVal($adoc2, "total_lanes");
+      print OUT "ANALYSIS:  $analysisDataURI \n";
+      print OUT "ANALYSISID: $analysisId\n";
+      print OUT "PARTICIPANT ID: $participantId\n";
+      print OUT "SAMPLE ID: $sampleId\n";
+      print OUT "ALIQUOTID: $aliquotId\n";
+      print OUT "SUBMITTER PARTICIPANT ID: $submitterParticipantId\n";
+      print OUT "SUBMITTER SAMPLE ID: $submitterSampleId\n";
+      print OUT "SUBMITTER ALIQUOTID: $submitterAliquotId\n";
       my $libName = getVal($adoc, 'LIBRARY_NAME'); #->getElementsByTagName('LIBRARY_NAME')->item(0)->getFirstChild->getNodeValue;
       my $libStrategy = getVal($adoc, 'LIBRARY_STRATEGY'); #->getElementsByTagName('LIBRARY_STRATEGY')->item(0)->getFirstChild->getNodeValue;
       my $libSource = getVal($adoc, 'LIBRARY_SOURCE'); #->getElementsByTagName('LIBRARY_SOURCE')->item(0)->getFirstChild->getNodeValue;
-      print "LibName: $libName LibStrategy: $libStrategy LibSource: $libSource\n";
+      print OUT "LibName: $libName LibStrategy: $libStrategy LibSource: $libSource\n";
       # get files
       # now if these are defined then move onto the next step
       if (defined($libName) && defined($libStrategy) && defined($libSource) && defined($analysisId) && defined($analysisDataURI)) { 
-        print "  gtdownload -c gnostest.pem -v -d $analysisDataURI\n";
+        print OUT "  gtdownload -c gnostest.pem -v -d $analysisDataURI\n";
         #system "gtdownload -c gnostest.pem -vv -d $analysisId\n";
-        print "\n";
-        $d->{$submitterParticipantId}{$submitterSampleId}{$submitterAliquotId}{analysis_id}{$analysisId} = 1; 
-        $d->{$submitterParticipantId}{$submitterSampleId}{$submitterAliquotId}{analysis_url}{$analysisDataURI} = 1; 
-        $d->{$submitterParticipantId}{$submitterSampleId}{$submitterAliquotId}{library_name}{$libName} = 1; 
-        $d->{$submitterParticipantId}{$submitterSampleId}{$submitterAliquotId}{library_strategy}{$libStrategy} = 1; 
-        $d->{$submitterParticipantId}{$submitterSampleId}{$submitterAliquotId}{library_source}{$libSource} = 1; 
+        print OUT "\n";
+#        $d->{$submitterParticipantId}{$submitterSampleId}{$alignment}{$submitterAliquotId}{$libName}{analysis_id}{$analysisId} = 1; 
+#        $d->{$submitterParticipantId}{$submitterSampleId}{$alignment}{$submitterAliquotId}{$libName}{analysis_url}{$analysisDataURI} = 1; 
+#        $d->{$submitterParticipantId}{$submitterSampleId}{$alignment}{$submitterAliquotId}{$libName}{library_name}{$libName} = 1; 
+#        $d->{$submitterParticipantId}{$submitterSampleId}{$alignment}{$submitterAliquotId}{$libName}{library_strategy}{$libStrategy} = 1; 
+#        $d->{$submitterParticipantId}{$submitterSampleId}{$alignment}{$submitterAliquotId}{$libName}{library_source}{$libSource} = 1; 
+#        $d->{$submitterParticipantId}{$submitterSampleId}{$alignment}{$submitterAliquotId}{$libName}{alignment_genome}{$alignment} = 1; 
+#        $d->{$submitterParticipantId}{$submitterSampleId}{$alignment}{$submitterAliquotId}{$libName}{use_control}{$use_control} = 1; 
+#        $d->{$submitterParticipantId}{$submitterSampleId}{$alignment}{$submitterAliquotId}{$libName}{total_lanes}{$total_lanes} = 1; 
+#        $d->{$submitterParticipantId}{$submitterSampleId}{$alignment}{$submitterAliquotId}{$libName}{submitter_participant_id}{$submitterParticipantId} = 1; 
+#        $d->{$submitterParticipantId}{$submitterSampleId}{$alignment}{$submitterAliquotId}{$libName}{submitter_sample_id}{$submitterSampleId} = 1; 
+#        $d->{$submitterParticipantId}{$submitterSampleId}{$alignment}{$submitterAliquotId}{$libName}{submitter_aliquot_id}{$submitterAliquotId} = 1; 
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{analysis_id}{$analysisId} = 1; 
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{analysis_url}{$analysisDataURI} = 1; 
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{library_name}{$libName} = 1; 
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{library_strategy}{$libStrategy} = 1; 
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{library_source}{$libSource} = 1; 
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{alignment_genome}{$alignment} = 1; 
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{use_control}{$use_control} = 1; 
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{total_lanes}{$total_lanes} = 1; 
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{submitter_participant_id}{$submitterParticipantId} = 1; 
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{submitter_sample_id}{$submitterSampleId} = 1; 
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{submitter_aliquot_id}{$submitterAliquotId} = 1; 
+# need to add
+# input_bam_paths=9c414428-9446-11e3-86c1-ab5c73f0e08b/hg19.chr22.5x.normal.bam
+# gnos_input_file_urls=https://gtrepo-ebi.annailabs.com/cghub/data/analysis/download/9c414428-9446-11e3-86c1-ab5c73f0e08b
+# gnos_input_metadata_urls=https://gtrepo-ebi.annailabs.com/cghub/metadata/analysisFull/9c414428-9446-11e3-86c1-ab5c73f0e08b
+
       } else {
-        print "ERROR: one or more critical fields not defined, will skip $analysisId\n\n";
+        print OUT "ERROR: one or more critical fields not defined, will skip $analysisId\n\n";
         next;
       }
       my $files = readFiles($adoc);
-      print "FILE:\n";
+      print OUT "FILE:\n";
       foreach my $file(keys %{$files}) {
-        print "  FILE: $file SIZE: ".$files->{$file}{size}." CHECKSUM: ".$files->{$file}{checksum}."\n";
-        print "  LOCAL FILE PATH: $analysisId/$file\n";
-        $d->{$submitterParticipantId}{$submitterSampleId}{$submitterAliquotId}{files}{$file}{size} = $files->{$file}{size}; 
-        $d->{$submitterParticipantId}{$submitterSampleId}{$submitterAliquotId}{files}{$file}{checksum} = $files->{$file}{checksum}; 
+        print OUT "  FILE: $file SIZE: ".$files->{$file}{size}." CHECKSUM: ".$files->{$file}{checksum}."\n";
+        print OUT "  LOCAL FILE PATH: $analysisId/$file\n";
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{files}{$file}{size} = $files->{$file}{size}; 
+        $d->{$participantId}{$sampleId}{$alignment}{$aliquotId}{$libName}{files}{$file}{checksum} = $files->{$file}{checksum}; 
         # URLs?
       }
 
@@ -139,11 +233,11 @@ sub read_sample_info {
   #$doc->printToFile ("out.xml");
   
   # Print to string
-  #print $doc->toString;
+  #print OUT $doc->toString;
   
   # Avoid memory leaks - cleanup circular references for garbage collection
   $doc->dispose;
-
+  close OUT;
   return($d);
 }
 
@@ -164,6 +258,7 @@ sub read_cluster_info {
     my $pass = $json->{$c}{password};
     my $web = $json->{$c}{webservice};
     my $acc = $json->{$c}{workflow_accession};
+    print "EXAMINING CLUSER: $c\n";
     #print "wget -O - --http-user=$user --http-password=$pass -q $web\n"; 
     my $info = `wget -O - --http-user='$user' --http-password=$pass -q $web/workflows/$acc`; 
     #print "INFO: $info\n";
@@ -175,22 +270,32 @@ sub read_cluster_info {
       my $wr = `wget -O - --http-user='$user' --http-password=$pass -q $web/workflows/$acc/runs`; 
       #print "WR: $wr\n";
       my $dom2 = XML::LibXML->new->parse_string($wr);
-      # find running samples
-      for my $node ($dom2->findnodes('//WorkflowRunList2/list/iniFile/text()')) {
-        my $ini_contents = $node->toString();
-        $ini_contents =~ /gnos_input_metadata_urls=(\S+)/;
-        $run_samples->{$1} = 1;
-      }
+
       # find available clusters
       my $running = 0;
+      print "\tWORKFLOWS ON THIS CLUSTER\n";
+      my $i=0;
       for my $node ($dom2->findnodes('//WorkflowRunList2/list/status/text()')) {
-        print "WORKFLOW: ".$acc." STATUS: ".$node->toString()."\n";
+        $i++;
+        print "\tWORKFLOW: ".$acc." STATUS: ".$node->toString()."\n";
         if ($node->toString() eq 'running' || $node->toString() eq 'scheduled' || $node->toString() eq 'submitted') { $running++; }
+        # find running samples
+        my $j=0;
+        for my $node2 ($dom2->findnodes('//WorkflowRunList2/list/iniFile/text()')) {
+          $j++;
+          my $ini_contents = $node2->toString();
+          $ini_contents =~ /gnos_input_metadata_urls=(\S+)/;
+          my @urls = split /,/, $1;
+          my $sorted_urls = join(",", sort @urls);
+          if ($i==$j) { $run_samples->{$sorted_urls} = $node->toString(); print "\t\tINPUTS: $sorted_urls\n"; }
+        }
       } 
       # if there are no running workflows on this cluster it's a candidate
       if ($running == 0) {
-        print "NO RUNNING WORKFLOWS, ADDING TO LIST OF AVAILABLE CLUSTERS\n";
+        print "\tNO RUNNING WORKFLOWS, ADDING TO LIST OF AVAILABLE CLUSTERS\n\n";
         $d->{$c} = $json->{$c}; 
+      } else {
+        print "\tCLUSTER HAS RUNNING WORKFLOWS, NOT ADDING TO AVAILABLE CLUSTERS\n\n";
       }
     }
   }
