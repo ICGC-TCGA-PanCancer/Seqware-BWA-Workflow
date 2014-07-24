5549935b2ff819d13972da230941ee5f7aeb3d95
Brian O'Connor <briandoconnor@gmail.com>
2014-07-24 18:06:26 -0400

    Need the path in there too

--- workflow/scripts/gnos_upload_data.pl
+++ workflow/scripts/gnos_upload_data.pl
@@ -141,7 +141,7 @@ sub upload_submission {
   }
 
   # we need to hack the manifest.xml to drop any files that are inputs and I won't upload again
-  modify_manifest_file("manifest.xml");
+  modify_manifest_file("$sub_path/manifest.xml");
 
   $cmd = "cd $sub_path; gtupload -v -c $key -u ./manifest.xml; cd -";
   if (!$test) {
